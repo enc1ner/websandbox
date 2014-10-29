ad86321721477615abca0467ca961305b9c45516
David Gr”nlund <enc1ner@gmail.com>
2014-10-29 18:47:31 +0100

    added autofac configuration for web project

---
+++ Trunk/WebSandBoxApplication.Web/App_Start/AutofacConfig.cs
@@ -0,0 +1,25 @@
+﻿using Autofac;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebSandBoxApplication.Web.App_Start
+{
+    public class AutofacConfig
+    {
+        public AutofacConfig()
+        {
+            ContainerBuilder builder = new ContainerBuilder();
+
+            RegisterServices(builder);
+
+            builder.Build();
+        }
+
+        private void RegisterServices(ContainerBuilder builder)
+        {
+
+        }
+    }
+}
